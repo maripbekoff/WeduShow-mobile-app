eb3a59449b5be68228fb9592577c2e4cbb658399
Timur Kim <allrighty1809@yandex.kz>
2020-06-26 20:18:00 +0600

    widgets dev

--- lib/ui/prepare_stream/widgets/widgets_view_widgets.dart
+++ lib/ui/prepare_stream/widgets/widgets_view_widgets.dart
@@ -11,12 +11,13 @@ class VictorineDialog extends StatefulWidget {
 }
 
 class _VictorineDialogState extends State<VictorineDialog> {
-  TextEditingController _questionController = TextEditingController();
-  List<TextEditingController> _textContollers =
-      List.generate(3, (index) => TextEditingController());
+  PageController _pageController = PageController();
+  List _textContollers = List.generate(
+      1, (index) => List.generate(4, (index) => TextEditingController()));
   int _groupValue;
   double _sliderValue = 5;
-  int _index;
+  int _listViewBuilderIndex;
+  int _pageViewBuilderIndex;
   List<int> _pageValue = [1];
 
   @override
@@ -24,10 +25,11 @@ class _VictorineDialogState extends State<VictorineDialog> {
     return Dialog(
       backgroundColor: Colors.white,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height / 2,
-        width: MediaQuery.of(context).size.width,
-        child: SingleChildScrollView(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height / 2,
+          width: MediaQuery.of(context).size.width,
           child: Column(
             children: <Widget>[
               Padding(
@@ -58,123 +60,180 @@ class _VictorineDialogState extends State<VictorineDialog> {
                 ),
               ),
               Divider(color: Colors.black45),
-              Form(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                        controller: _questionController,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.all(8),
-                          hintText: "Вопрос",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black12,
-                            ),
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black12,
-                            ),
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 25),
-                      Container(
-                        height: 200,
-                        child: ListView.separated(
-                          primary: false,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemCount: _textContollers.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(30),
-                                border: Border.all(color: Colors.black12),
-                              ),
-                              child: Row(
-                                children: <Widget>[
-                                  Flexible(
-                                    child: TextFormField(
-                                      controller: _textContollers[index],
-                                      decoration: InputDecoration(
-                                        contentPadding: EdgeInsets.all(8),
-                                        hintText: "Ответ",
-                                        border: InputBorder.none,
-                                      ),
-                                    ),
+              Expanded(
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: _pageValue.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    _pageViewBuilderIndex = index;
+                    return SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              controller: _textContollers[index][0],
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.all(8),
+                                hintText: "Вопрос",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black12,
                                   ),
-                                  Radio(
-                                    value: index,
-                                    groupValue: _groupValue,
-                                    onChanged: (int value) {
-                                      print(value);
-                                      setState(() {
-                                        _groupValue = value;
-                                      });
-                                    },
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black12,
                                   ),
-                                ],
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
                               ),
-                            );
-                          },
-                          separatorBuilder: (BuildContext context, int index) =>
-                              SizedBox(height: 15),
-                        ),
-                      ),
-                      SizedBox(height: 25),
-                      Text("Время на ответ"),
-                      Slider(
-                        value: _sliderValue,
-                        onChanged: (double value) {
-                          setState(() {
-                            _sliderValue = value;
-                          });
-                        },
-                        min: 5,
-                        max: 15,
-                        divisions: 2,
-                        label: "${_sliderValue.toInt()} сек.",
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          SizedBox(
-                            height: 50,
-                            width: 50,
-                            child: ListView.builder(
-                                shrinkWrap: true,
+                            ),
+                            SizedBox(height: 25),
+                            Container(
+                              height: 200,
+                              child: ListView.separated(
                                 primary: false,
                                 physics: NeverScrollableScrollPhysics(),
-                                scrollDirection: Axis.horizontal,
-                                itemCount: _pageValue.length,
+                                itemCount: 3,
                                 itemBuilder: (BuildContext context, int index) {
-                                  _index = index + 2;
-                                  return IconButton(
-                                    onPressed: () {},
-                                    icon: Text("${_pageValue[index]}"),
+                                  return Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(30),
+                                      border: Border.all(color: Colors.black12),
+                                    ),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Flexible(
+                                          child: TextFormField(
+                                            controller: _textContollers[
+                                                    _pageViewBuilderIndex]
+                                                [index + 1],
+                                            decoration: InputDecoration(
+                                              contentPadding: EdgeInsets.all(8),
+                                              hintText: "Ответ",
+                                              border: InputBorder.none,
+                                            ),
+                                          ),
+                                        ),
+                                        Radio(
+                                          value: index,
+                                          groupValue: _groupValue,
+                                          onChanged: (int value) {
+                                            print(value);
+                                            setState(() {
+                                              _groupValue = value;
+                                            });
+                                          },
+                                        ),
+                                      ],
+                                    ),
                                   );
-                                }),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.add),
-                            onPressed: () {
-                              _pageValue.add(_index);
-                              print(_pageValue);
-                              setState(() {});
-                            },
-                          ),
-                        ],
+                                },
+                                separatorBuilder:
+                                    (BuildContext context, int index) =>
+                                        SizedBox(height: 15),
+                              ),
+                            ),
+                            SizedBox(height: 25),
+                            Text("Время на ответ"),
+                            Slider(
+                              value: _sliderValue,
+                              onChanged: (double value) {
+                                setState(() {
+                                  _sliderValue = value;
+                                });
+                              },
+                              min: 5,
+                              max: 15,
+                              divisions: 2,
+                              label: "${_sliderValue.toInt()} сек.",
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 50,
+                                  child: ListView.builder(
+                                    shrinkWrap: true,
+                                    primary: false,
+                                    physics: NeverScrollableScrollPhysics(),
+                                    scrollDirection: Axis.horizontal,
+                                    itemCount: _pageValue.length,
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      _listViewBuilderIndex = index + 2;
+                                      return Center(
+                                        child: InkWell(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          onTap: () {},
+                                          child: Padding(
+                                            padding: EdgeInsets.all(
+                                                _pageValue.length == 10
+                                                    ? 6.8
+                                                    : 6.1),
+                                            // 9 - 6.8
+                                            // ...10 - 6.1
+                                            child: Text(
+                                              "${_pageValue[index]}",
+                                              style: TextStyle(
+                                                fontSize: 18,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                                _pageValue.length <= 9
+                                    ? Row(
+                                        children: <Widget>[
+                                          IconButton(
+                                            padding: EdgeInsets.zero,
+                                            icon: Icon(Icons.add),
+                                            onPressed: () {
+                                              _pageValue.add(index + 2);
+                                              print(_pageValue);
+                                              setState(() {});
+                                            },
+                                          ),
+                                          _pageValue.length == 1
+                                              ? SizedBox()
+                                              : IconButton(
+                                                  padding: EdgeInsets.zero,
+                                                  icon: Icon(Icons.delete,
+                                                      color: Colors.red),
+                                                  onPressed: () {
+                                                    _pageValue.removeLast();
+                                                    print(_pageValue);
+                                                    setState(() {});
+                                                  },
+                                                ),
+                                        ],
+                                      )
+                                    : IconButton(
+                                        padding: EdgeInsets.zero,
+                                        icon: Icon(Icons.delete,
+                                            color: Colors.red),
+                                        onPressed: () {
+                                          _pageValue.removeLast();
+                                          print(_pageValue);
+                                          setState(() {});
+                                        },
+                                      ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
               ),
             ],
